3d4206178dd9424c3fdd74ce75720dd4cb61487f
NVK Chaitanya <nvk.681@gmail.com>
2021-04-14 12:36:03 +0530

    add: radd, mul, rmul, neg operations for vectors

--- python/vectors.py
+++ python/vectors.py
@@ -27,6 +27,44 @@ class Vector:
             result[j] = self[j] + other[j]
         return result
 
+    def __radd__(self, other):
+        """Return sum of two vectors, when vector is on thr right of the operation"""
+        if len(self) != len(other):
+            raise ValueError("Dimensions must be equal")
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    def __neg__(self):
+        """Return negative/reverse of the vector."""
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = -self[j]
+        return result
+
+    def __mul__(self, other):
+        """Return multiplication of two vectors."""
+        self_lenght = len(self)
+        result = Vector(self_lenght)
+        if type(other) is int:
+            for j in range(self_lenght):
+                result[j] = self[j] * other
+        else:
+            if self_lenght == len(other):
+                for item in range(self_lenght):
+                    result[item] = self[item] * other[item]
+            else:
+                raise ValueError("Dimensions must be equal")
+        return result
+
+    def __rmul__(self, other):
+        """Return multiplication of two vectors, when vector is on thr right of the operation"""
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] * other
+        return result
+
     def __eq__(self, other):
         """Return True if vector has same coordinates as other."""
         return self._coordinates == other._coordinates
@@ -34,3 +72,13 @@ class Vector:
     def __ne__(self, other):
         """Return True if vector differs from other."""
         return not self == other
+
+test_vector = Vector(3)
+test_vector_other = Vector(3)
+test_vector[0] = test_vector_other[2] = 4
+test_vector[1] = test_vector_other[1] = 5
+test_vector[2] = test_vector_other[0] = 6
+test_vector = 3*test_vector
+print(test_vector[0])
+print(test_vector[1])
+print(test_vector[2])
