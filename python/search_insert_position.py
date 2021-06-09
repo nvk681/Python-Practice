10b09d9ce2e8a0ef7ecf9b6aac45e58e2aea7f9d
NVK Chaitanya <nvk.681@gmail.com>
2021-06-09 18:40:26 +0530

    add: search and insert position

---
+++ python/search_insert_position.py
@@ -0,0 +1,20 @@
+def binary_search(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+        else:
+            return binary_search(arr, mid + 1, high, x)
+    else:
+        return high+1
+
+def searchInsert(nums, target):
+    return binary_search(nums, 0, len(nums)-1, target)
+
+print(searchInsert([1,3,5,6], 5))
+print(searchInsert([1,3,5,6], 2))
+print(searchInsert([1,3,5,6], 7))
+print(searchInsert([1,3,5,6], 0))
+print(searchInsert([1], 0))
