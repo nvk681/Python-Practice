c202269c84a3146935d2b8ad85bd5116ae649fef
NVK Chaitanya <nvk.681@gmail.com>
2021-07-05 19:55:02 +0530

    add: wildcard

--- python/wildcard_matching.py
+++ python/wildcard_matching.py
@@ -1,17 +1,36 @@
+def strrmatch(strr, pattern, n, m):
+ 
+    if (m == 0):
+        return (n == 0)
+ 
+    lookup = [[False for i in range(m + 1)] for j in range(n + 1)]
+ 
+    lookup[0][0] = True
+ 
+    for j in range(1, m + 1):
+        if (pattern[j - 1] == '*'):
+            lookup[0][j] = lookup[0][j - 1]
+ 
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if (pattern[j - 1] == '*'):
+                lookup[i][j] = lookup[i][j - 1] or lookup[i - 1][j]
+            elif (pattern[j - 1] == '?' or strr[i - 1] == pattern[j - 1]):
+                lookup[i][j] = lookup[i - 1][j - 1]
+            else:
+                lookup[i][j] = False
+ 
+    return lookup[n][m]
+
 def isMatch( s: str, p: str) -> bool:
-        if not p:
-            return not s
-        first_match = bool(s) and p[0] in {s[0], '?', '*'}
+    return strrmatch(s,p,len(s),len(p))
 
-        if len(p) >= 2 and p[1] == '*':
-            return first_match and isMatch(s[1:], p)
-        else:
-            return first_match and isMatch(s[1:], p[1:])
-
-print(isMatch("aa", "a"))
-print(isMatch("aa", "*"))
-print(isMatch("cb", "?a"))
-print(isMatch("adceb", "a*b"))
-print(isMatch("adceb", "a*c?b"))
-# print(isMatch("aa", "a"))
-# print(isMatch("aa", "a"))
+print(False == isMatch("b","*?*?*"))
+print(False == isMatch("", "?"))
+print(True == isMatch("", "****"))
+print(True == isMatch("aa", "*"))
+print(False == isMatch("cb", "?a"))
+print(True == isMatch("adceb", "a*b"))
+print(True == isMatch("adceb", "a*c?b"))
+print(False == isMatch("aab", "c*a*b"))
+print(False == isMatch("aa", "a"))
